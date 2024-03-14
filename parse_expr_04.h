3d4d46dbe98d73b20f0431eba3ecb7b418836b6e
pawel <plapacz6@gmail.com>
2024-03-14 17:42:20 +0100

    refac

--- parse_expr_04.h
+++ parse_expr_04.h
@@ -3,11 +3,18 @@ Copyright 2020-2024 plapacz6@gmail.com
 
 This file is part of math_parser_01.
 
-math_parser_01 is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+math_parser_01 is free software: you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
 
-math_parser_01 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+math_parser_01 is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+details.
 
-You should have received a copy of the GNU Lesser General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+You should have received a copy of the GNU Lesser General Public License along
+ with math_parser_01. If not, see <https://www.gnu.org/licenses/>.
 */
 
 #ifndef PARSE_EXPR_04_H_INCLUDED
